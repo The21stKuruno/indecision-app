f1ecf23480fac7b8e23cea674b287dcb814ac7db
iamcrisaian <The21stKuruno@yahoo.com>
2018-02-18 23:52:39 +0900

    remove css folder and added counter-example.js

--- src/app.js
+++ src/app.js
@@ -19,51 +19,5 @@ const template = (
   </div>
 );
 
-// const user = {
-//   name: 'Cris',
-//   age: 24,
-//   location: 'Daejeon, South Korea'
-// };
-//
-// function getLocation(location) {
-//   if (location) {
-//     return <p>Location: {location}</p>
-//   }
-// }
-//
-// const templateTwo = (
-//   <div>
-//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
-//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-//     {getLocation(user.location)}
-//   </div>
-// );
-
-let count = 0;
-
-const addOne = () => {
-  console.log('addOne');
-};
-
-const minusOne = () => {
-  console.log('minusOne');
-};
-
-const reset = () => {
-  console.log('reset');
-};
-
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>reset</button>
-  </div>
-);
-
-
-
 const appRoot = document.getElementById('app');
-
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
