94a7ad1891523cb05943e02f72bdfa8f01e2fc24
iamcrisaian <The21stKuruno@yahoo.com>
2018-02-21 23:26:47 +0900

    picking an option

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,6 +26,11 @@ var removeAll = function removeAll() {
   app.options = [];
   renderAddItemApp();
 };
+var onMakeDecision = function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randomNum];
+  alert(option);
+};
 
 var renderAddItemApp = function renderAddItemApp() {
   var template = React.createElement(
@@ -47,9 +52,9 @@ var renderAddItemApp = function renderAddItemApp() {
       app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
-      'p',
-      null,
-      app.options.length
+      'button',
+      { disabled: app.options.length === 0, onClick: onMakeDecision },
+      'What should I do'
     ),
     React.createElement(
       'button',
--- src/app.js
+++ src/app.js
@@ -24,6 +24,11 @@ const removeAll = () => {
   app.options = [];
   renderAddItemApp();
 };
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
 
 const renderAddItemApp = () => {
   const template = (
@@ -31,7 +36,7 @@ const renderAddItemApp = () => {
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <p>{app.options.length}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do</button>
       <button onClick={removeAll}>Remove All</button>
       <ol>
         {
