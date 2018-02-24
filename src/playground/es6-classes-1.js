3fd1000c5ccf41e000f9ac63636e2ff696d674e7
iamcrisaian <The21stKuruno@yahoo.com>
2018-02-24 22:34:48 +0900

    es6 classes 2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,8 +11,44 @@ class Person {
   }
 }
 
-const me = new Person('Cris-Aian Vergara', 24);
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, location) {
+    super(name, age);
+    this.location = location;
+  }
+  homeLocation() {
+    return !!this.location;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.homeLocation()) {
+      greeting += ` I'm visiting from ${this.location}.`;
+    }
+    return greeting;
+  }
+}
+
+const me = new Traveler('Cris-Aian Vergara', 24, 'Daejeon');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
+console.log(other.getGreeting());
