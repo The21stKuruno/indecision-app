3c26fb41fabcce353debb730391ba8e27ffa7e8d
iamcrisaian <The21stKuruno@yahoo.com>
2018-03-02 22:29:32 +0900

    Build it visible updated

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,32 +1,27 @@
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
+        {this.state.visibility && <p>Hey! There are some details you can now see.</p>}
+      </div>
+    );
+  }
+}
 
-const appRoot = document.getElementById('app');
-
-const listNote = {
-  note: []
-};
-
-const showIt = () => {
-  const putNote = 'Wiwoo! What\'s Poppin?';
-  listNote.note.push(putNote);
-  console.log(listNote.note);
-  render();
-};
-
-const hideIt = () => {
-  listNote.note = [];
-  console.log(listNote.note);
-  render();
-};
-
-const render = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      {listNote.note.length > 0 ? <button onClick={hideIt}>Hide Note</button> : <button onClick={showIt}>Show Note</button>}
-      {listNote.note && <p>{listNote.note}</p>}
-    </div>
-  );
-  ReactDOM.render(template, appRoot)
-};
-
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
