3b32850b883dee01716a01ce8106a56eca1978fd
iamaian <The21stKuruno@yahoo.com>
2018-03-25 23:50:27 +0900

    separted each components

--- src/person.js
+++
@@ -1,7 +0,0 @@
-console.log('person is running');
-
-const isAdult = (age) => age >= 18;
-const canDrink = (drink) => drink >= 21;
-const isSenior = (age) => age >= 65;
-
-export { isAdult, canDrink, isSenior as default };
